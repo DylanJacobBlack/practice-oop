f6ecef42776f7e60f49186d05c0b8fdf22314f37
Dylan Black <dylan.jacob.black@gmail.com>
2022-02-19 16:36:58 -0600

    Add switch logic

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,18 +1,46 @@
-class App {
-
-}
-
-
-class ProjectsList {
-
-}
+class ToolTip {}
 
 class ProjectItem {
+  constructor(id) {
+    this.id = id;
+    this.connectMoreInfoButton();
+    this.connectSwitchButton();
+  }
 
+  connectMoreInfoButton();
+  connectSwitchButton() {
+    const projectItemElement = document.getElementById(this.id);
+    const switchBtn = projectItemElement.querySelector('button:last-of-type');
+    switchBtn.addEventListener('click', switchButtonHandler)
+  };
 }
 
-class ToolTip {
-  static init()
+class ProjectsList {
+  projects = [];
+
+  constructor(type, switcherHandlerFunction) {
+    this.type = type;
+    this.switchHandler = switcherHandlerFunction;
+    const prjItems = document.querySelectorAll(`#${type}-projects li`);
+    for (const prjItem of prjItems) {
+      this.projects.push(new ProjectItem(prjItem.id));
+    }
+    console.log(this.projects);
+  }
+
+  addProject() {}
+
+  switchProject(projectId) {
+    this.switchHandler(this.projects.find(project => project.id = projectId))
+    this.projects = this.projects.filter(project => project.id !== project.id);
+  }
 }
 
-App.init()
+class App {
+  static init() {
+    const activeProjectsList = new ProjectsList("active");
+    const finishedProjectsList = new ProjectsList("finished");
+  }
+}
+
+App.init();
